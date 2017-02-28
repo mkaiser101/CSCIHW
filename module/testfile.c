ee6c1720b669be2e04ba7a5aa5e6b66e11ecb6af
mkaiser101 <mkaiser101@gmail.com>
2017-02-28 14:32:23 -0700

    Updated testfile for requirement
    Also added hidden 'q' function

--- module/testfile.c
+++ module/testfile.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#define DEVICE "/dev/simple_character_device"
-#define BUFFER_SIZE 1024
+#include <fcntl.h>  //open
+#include <unistd.h> //close
+#define SimpleDEVICE "/dev/simple_character_device"
+#include <string.h>
+
 
 int main () {
-	char command, buffer[BUFFER_SIZE];
-	int file = open(DEVICE, O_RDWR);
+	char command;
+	char buffer[1024];
+	int Drivefile = open(SimpleDEVICE, O_RDWR);
 	while (1) {
 		printf("\nr) Read from device\nw) Write to device\ne) Exit device\nAnything else to continue reading and writing\n\nEnter command: ");
 		scanf("%c", &command);
 		switch (command) {
-			case 'w':
-			case 'W':
+			case 'w': 
 				printf("Enter data you want to write to the device: ");
 				scanf("%s", buffer);
-				write(file, buffer, BUFFER_SIZE);
+				write(Drivefile, buffer, strlen(buffer));
 				while (getchar() != '\n');
 				break;
-			case 'r':
-			case 'R':
-				read(file, buffer, BUFFER_SIZE);
+			case 'r': 
+				read(Drivefile, buffer, 1024);
 				printf("Device output: %s\n", buffer);
 				while (getchar() != '\n');
 				break;
 			case 'e':
-			case 'E':
+				return 0;
+			case 'q':
+				printf("the game is over");
+				printf("the game is over");
+				printf("the game is over");
+				printf("the game is over");
+				printf("the game is over");
+				printf("the game is over");
 				return 0;
 			default:
 				while (getchar() != '\n');
 		}
 	}
-	close(file);
+	close(Drivefile);
 	return 0;
 }
+
