4847226db6d2a49e3c6b399a00d6c00b1e9ac303
mkaiser101 <mkaiser101@gmail.com>
2017-02-28 14:31:28 -0700

    Now it is functional to requirements
    Fixed my dmesg output with a few changes

--- module/simple_char_driver.c
+++ module/simple_char_driver.c
@@ -4,80 +4,35 @@
 #include<linux/fs.h>
 #include<asm/uaccess.h>
 #define BUFFER_SIZE 1024
+#define MAJOR_NUMBER 255
+#define DEV_NAME "simple_character_device"
 
+static char device_buffer[BUFFER_SIZE];
+static int openCount = 0;
+static int closeCount = 0;
 
-
-
-#define deviceName "simple_character_device"
-static char device_buffer[BUFFER_SIZE] = {'\0'};
-static int numberOfTimesFileWasOpened = 0;
-static int numberOfTimesFileWasClosed = 0;
-
-
-static int availableSpace = 0;
-static int bytesNotCopied = 0;
-static int totalBytesRead = 0;
-static int bytesToWrite = 0;
-static int totalBytesCopied = 0;
 ssize_t simple_char_driver_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
 {
-	{
 	/* *buffer is the userspace buffer to where you are writing the data you want to be read from the device file*/
 	/*  length is the length of the userspace buffer*/
 	/*  current position of the opened file*/
-	/* copy_to_user function. source is device_buffer (the buffer defined at the start of the code) and destination is the userspace buffer *buffer */
-
-	 /* note: *offset seems to always be zero but included it just in case */
-	 bytesNotCopied = copy_to_user(buffer, device_buffer, *offset);
-	 totalBytesRead = *offset - bytesNotCopied;
-
-	 /* Nothing was read */
-	 if(totalBytesRead == 0){
-		 printk(KERN_ALERT "Unable to read buffer, it is either empty or there was an error!\n");
-		 return totalBytesRead;
-	 }
-
-
-	 printk(KERN_ALERT "Read %d bytes; Buffer Message: %s\n", totalBytesRead, device_buffer);
-
+	/* copy_to_user function. source is device_buffer (the buffer defined at the start of the code) and destination is the userspace 		buffer *buffer */
+	printk(KERN_ALERT "Reading from device.\n");
+	copy_to_user(buffer, device_buffer, length);
 	return 0;
-}
 }
 
 
 
 ssize_t simple_char_driver_write (struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
 {
-
 	/* *buffer is the userspace buffer where you are writing the data you want to be written in the device file*/
 	/*  length is the length of the userspace buffer*/
 	/*  current position of the opened file*/
 	/* copy_from_user function. destination is device_buffer (the buffer defined at the start of the code) and source is the userspace 		buffer *buffer */
-
-	 /* note: *offset seems to always be zero but included it just in case */
-	 availableSpace = BUFFER_SIZE - *offset;
-
-	 /* Check availableSpace */
-	 if(availableSpace <= length){ /*There is insufficient space for the copy */
-		 bytesToWrite = availableSpace; /* Only copy until space is full */
-	 }else{ /* All bytes can be copied */
-	 	 bytesToWrite = length;
-	 }
-
-	 bytesNotCopied = copy_from_user(device_buffer + *offset, buffer, bytesToWrite);
-	 totalBytesCopied = bytesToWrite - bytesNotCopied;
-
-	 /* if the bytes not coppied equals bytesToCopy, none of the bytes were copied so the device is full */
-	 if(totalBytesCopied == 0){
-		 printk(KERN_ALERT "Could not copy message, device is full!\n");
-		 return -1;
-	 };
-
-	 /* current location on buffer*/
-	 *offset += totalBytesCopied;
-
-	 printk(KERN_ALERT "Wrote %d bytes!\n", totalBytesCopied);
-
+	printk(KERN_ALERT "Writing to device.\n");
+	copy_from_user(device_buffer, buffer, length);
+	printk(KERN_ALERT "Wrote %d bytes to device.\n", length);
 	return length;
 }
 
@@ -85,8 +40,8 @@ ssize_t simple_char_driver_write (struct file *pfile, const char __user *buffer,
 int simple_char_driver_open (struct inode *pinode, struct file *pfile)
 {
 	/* print to the log file that the device is opened and also print the number of times this device has been opened until now*/
-	numberOfTimesFileWasOpened++;
-	printk(KERN_ALERT "The file has been opened. It has been opened %d times now.\n", numberOfTimesFileWasOpened);
+	openCount++;
+	printk(KERN_ALERT "Opened Device. Count: %d\n", openCount);
 	return 0;
 }
 
@@ -94,27 +49,27 @@ int simple_char_driver_open (struct inode *pinode, struct file *pfile)
 int simple_char_driver_close (struct inode *pinode, struct file *pfile)
 {
 	/* print to the log file that the device is closed and also print the number of times this device has been closed until now*/
-	numberOfTimesFileWasClosed++;
-	printk(KERN_ALERT "The file has been closed. It has been closed %d times now.\n", numberOfTimesFileWasClosed);
+	closeCount++;
+	printk(KERN_ALERT "Closed Device. Count: %d\n", closeCount);
 	return 0;
 }
 
 struct file_operations simple_char_driver_file_operations = {
 
-	.owner   = THIS_MODULE,
+	.owner = THIS_MODULE,
+	/* add the function pointers to point to the corresponding file operations. look at the file fs.h in the linux souce code*/
 	.read = simple_char_driver_read,
 	.write = simple_char_driver_write,
 	.open = simple_char_driver_open,
-	.release = simple_char_driver_close
-	/* add the function pointers to point to the corresponding file operations. look at the file fs.h in the linux souce code*/
+	.release = simple_char_driver_close,
 };
 
 static int simple_char_driver_init(void)
 {
 	/* print to the log file that the init function is called.*/
 	/* register the device */
-	register_chrdev(255, deviceName, &simple_char_driver_file_operations);
-	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
+	printk(KERN_ALERT "Initializing device.\n");
+	register_chrdev(MAJOR_NUMBER, DEV_NAME, &simple_char_driver_file_operations);
 	return 0;
 }
 
@@ -122,11 +77,10 @@ static int simple_char_driver_exit(void)
 {
 	/* print to the log file that the exit function is called.*/
 	/* unregister  the device using the register_chrdev() function. */
-	unregister_chrdev(255, deviceName);
-	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
+	printk(KERN_ALERT "Removing device.\n");
+	unregister_chrdev(MAJOR_NUMBER, DEV_NAME);
 	return 0;
 }
-
 
 /* add module_init and module_exit to point to the corresponding init and exit function*/
 module_init(simple_char_driver_init);
